4791fb39a6af611bf08edfd4c9c474b22a738045
taveiramtc <55445145+taveiramtc@users.noreply.github.com>
2019-10-03 08:13:30 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,5 @@
+if('serviceWorker' in navigator){
+	navigator.serviceWorker.register('../sw.js')
+		.then((reg) => console.log('Service worker registrado'))
+		.catch((err) => console.log('Service worker não registrado' , err));
+}
