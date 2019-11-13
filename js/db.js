7933beed69f7d8ed5da50bfd4db015693d8217f5
taveiramtc <55445145+taveiramtc@users.noreply.github.com>
2019-11-13 14:12:48 -0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,1 +1,18 @@
+db.enablePersistence()
+    .catch(function(err) {
+        if (err.code == 'failed-precondition') {
+            console.log('persistance failed');
+        } else if (err.code == 'unimplemented') {
+            console.log('persistance not available');
+        }
+    });
 
+db.collection('sobremesas').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+        if (change.type === 'added') {
+            renderRecipe(change.doc.data(), change.doc.id);
+        }
+        if (change.type === 'removed') {
+        }
+    });
+});
